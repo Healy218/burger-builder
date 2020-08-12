b9afbad58184ddb6a6be168602ec5688d37eb1a4
healy218 <mrhealy115@gmail.com>
2020-08-12 13:36:28 -0500

    Moving further along in burger builder

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -10,7 +11,34 @@ class ContactData extends Component {
         address: {
             street: '',
             postal: ''
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Max Schwarzmüller',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
         }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false});
+            })
+            .catch(error => {
+                this.setState({ loading: false});
+            });
     }
     
     render () {
@@ -22,7 +50,7 @@ class ContactData extends Component {
                     <input className={classes.Input} type="text" name="email" placeholder="Your email" />
                     <input className={classes.Input} type="text" name="street" placeholder="Your street" />
                     <input className={classes.Input} type="text" name="postal" placeholder="Your postal code" />
-                    <Button btnType="Sucess">Order</Button>
+                    <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
                 </form>
             </div>
         );
