390738bc364b7429f9f138d3576d418d6caa911e
healy218 <mrhealy115@gmail.com>
2020-08-17 17:10:58 -0500

    Added dynamic orderform

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -48,14 +48,14 @@ class ContactData extends Component {
                     },
                     value: ''
                 },
-            },
             deliveryMethod: {
-                elementType: 'input',
+                elementType: 'select',
                 elementConfig: {
                     options: [{value: 'fastest', displayValue: 'Fastest'},
                               {value: 'cheapest', displayValue: 'Cheapest'}]
                 },
                 value: ''
+            }
             },
         loading: false
     }
@@ -76,14 +76,32 @@ class ContactData extends Component {
                 this.setState({ loading: false});
             });
     }
+
+    inputChangedHandler = (event, ) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+    }
     
     render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                    <Input inputype = "input" type="text" name="name" placeholder="Your name" />
-                    <Input inputtype = "input" type="text" name="email" placeholder="Your email" />
-                    <Input inputtype = "input" type="text" name="street" placeholder="Your street" />
-                    <Input inputtype = "input" type="text" name="postal" placeholder="Your postal code" />
+                    {formElementsArray.map(formElement =>(
+                        <Input 
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
+                    ))}
                     <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
                 </form>
         );
